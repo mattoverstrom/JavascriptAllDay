596d4f974b23a892cb653b6eb0bc1ee4eb36d986
mattyo <matthewoverstrom@gmail.com>
2021-01-25 00:43:56 -0500

    bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Create The Application
+| Create The Applicationapp
 |--------------------------------------------------------------------------
 |
 | The first thing we will do is create a new Laravel application instance
